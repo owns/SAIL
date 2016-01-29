37bfccafb30e0bbe055fd6d2b885b15d18ed41c3
owns <owns13927@yahoo.com>
2016-01-29 15:50:14 -0600

    command line integrated into the GUI.  if no args, runs normal! Added the optional <model> argument.

--- src/frontend/GUI.java
+++ src/frontend/GUI.java
@@ -12,12 +12,13 @@
  */
 package frontend;
 
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -33,7 +34,9 @@ public class GUI extends Application {
 	public static final String configDefaultsFile = "./data/config.defaults.properties";
 	public static final String licenseFile = "./data/license.txt";
 	public GUIController controller;
-    @Override
+	private int exitCode = 1;
+	private String curModel = null;
+	@Override
     public void start(Stage stage) throws Exception {
     	stage.setTitle("SAIL (Sentiment Analysis and Incremental Learning)");
     	final FXMLLoader loader = new FXMLLoader(
@@ -61,31 +64,135 @@ public class GUI extends Application {
         //Scene scene = new Scene(root, 790, 600);
         stage.setResizable(true);
         stage.setScene(scene);
-        stage.show();
         
+        //======================================================================
+        // Handle arguments
+        //======================================================================
+        // print args to console
+        List<String> args = getParameters().getRaw();
+        System.out.format("--- %02d args: ---%n",args.size());
+        int c = 0;
+        for(String a: args){System.out.format("%d. %s%n",++c,a);}
+        System.out.println("----------------");
         
+        // .show() if no args, else handle!
+		String inpath = null;
+        String outpath = null;
+        String config = null;
+        switch(args.size()){
+        default:
+        	System.err.format("%sToo many arguments... just using first 3... %s", args.toArray().toString(),args.toString());
+        case 3:
+        	config = args.get(2);
+        case 2:
+        	outpath = args.get(1);
+        case 1:
+        	inpath = args.get(0);
+        	break;
+        case 0:
+        	exitCode = 0; stage.show(); return;
+        }
+ 		
+ 		// 01 set input folder
+ 		System.out.format("Input  Folder: \"%s\"%n",inpath);
+ 		// is dir and exists?
+ 		File a = new File(inpath);
+ 		if(!a.isDirectory()){ 
+ 			System.err.println("INPUT FOLDER DNE OR IS NOT A DIRECTORY!!!");
+ 			exitCode = 3;
+ 			Platform.exit(); return;
+ 		}
+ 		// are there files for us to process?
+ 		if(a.listFiles().length==0){
+ 			System.err.println("INPUT FOLDER IS EMPTY... SUCCESS!");
+ 			exitCode = 0; Platform.exit(); return; // stop
+ 		}
+ 		controller.setInputFolder(inpath);
+ 		
+ 			
+ 		// 02 set output folder
+ 		if(outpath != null){
+ 			System.out.format("Output Folder: \"%s\"%n",outpath);
+	 		// does it exist and is it a directory? 
+	 		if(!(new File(outpath).isDirectory())){
+	 			System.err.println("OUTPUT FOLDER DNE OR IS NOT A DIRECTORY!!!");
+	 			exitCode = 3; Platform.exit(); return; // stop
+	 		}
+ 		} else {
+ 			System.out.format("Output Folder: \"%s\"%n",outpath);
+ 			outpath = inpath;
+ 		}
+ 		controller.setOutputFolder(outpath);
+
+ 		// set config - assume model for now
+ 		// TODO determine what kind of param / add way to set more config...
+ 		if(config == null){controller.restoreDefaultProperties();}
+ 		else{
+ 			if(!(new File(config)).isFile()){
+ 				System.out.println("config not a file! "+config);
+ 				exitCode = 3; Platform.exit(); return; // stop
+ 			} else {
+ 				// check file is readable
+ 				java.io.InputStream b;
+ 				try {b = new FileInputStream(config);b.close();}
+ 				catch (IOException e) {
+ 					//e.printStackTrace();
+ 					System.out.format("config file cannot be read - %s. %s%n",e.getMessage(),config);
+ 					exitCode = 3; Platform.exit(); return;
+ 					}
+ 				finally {b = null;}
+ 				
+ 				curModel = controller.getProperty("model");
+ 				controller.setProperty("model",config);
+ 			}
+ 		}
+ 		
+ 		// click the next button!
+ 		Thread mythread = controller.processInput(new Runnable() {
+            @Override public void run() {
+            	System.err.println("Success!");
+            	exitCode = 0;
+            	Platform.exit(); return;
+            }
+        },new Runnable() {
+            @Override public void run() {
+        		System.err.println("Task Failed");
+        		exitCode = 2;
+        		Platform.exit(); return;
+        	}
+        });
+ 		
+ 		//start!
+ 		mythread.join();
+ 		exitCode = 0; Platform.exit(); return; // in case somehow a Runnable isn't called
     }
     
     @Override
     public void stop() throws Exception {
-    	// TODO Auto-generated method stub
+    	// roll back config if changed
+    	if(curModel != null){
+    		System.out.format("rolling back change to model in config file...");
+    		controller.setProperty("model",curModel);
+		}
     	controller.exit();
     	super.stop();
+    	System.out.format("exit code: %d %n",exitCode);
+    	System.exit(exitCode);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	try {
+    	/*try {
 			System.setOut(new PrintStream("log.stdout.txt"));
 			System.setErr(new PrintStream("log.stderr.txt"));
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	System.out.println("This is the first output to the log.stdout.txt file");
     	System.err.println("This is the first output to the log.stderr.txt file");
+    	//*/
         launch(args);
     }
     
